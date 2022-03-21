f70178090137da3bc7ae2f71340be29bc95460f9
dennis1900 <dennis.prichodko59@gmail.com>
2022-03-21 11:56:25 +0100

    Store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,14 @@
 import data from './data';
-import {createStore, creatStore} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk'
+import {productListReducer} from './reducers/productReducers';
 
 const initialState = {};
-const reducer = (state, action) => {
-    return {products: data.products};
-};
+const reducer = combineReducers({
+    productList: productListReducer,
 
-const store = createStore(reducer, initialState);
+});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
