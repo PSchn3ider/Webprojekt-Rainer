c5270084bae59847a8b0ab9e214ae0ff94bf1aee
dennis1900 <dennis.prichodko59@gmail.com>
2022-03-21 06:22:09 +0100

    ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div> 
-        </header>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+          </header>
         <main>
           <Routes>
           <Route path="/product/:id" element={<ProductScreen/>}></Route>
