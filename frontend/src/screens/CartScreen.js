870fe7fab871cd6ffe3331ae0d9ddbac25ca9d91
dennis1900 <dennis.prichodko59@gmail.com>
2022-03-22 10:23:57 +0100

    remove from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 
 export default function CartScreen(props){
@@ -23,7 +23,7 @@ export default function CartScreen(props){
     }, [dispatch, productId, qty]);
 
     const removeFromCartHandler = (id) => {
-
+      dispatch(removeFromCart(id));
     };
 
     const checkoutHandler = () => {
